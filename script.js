768a1886b810a9652f21668e01c7f3fb75c167ed
rabin sedhai <bisheshsedhai12@gmail.com>
2024-05-27 16:02:48 +0545

    added more music

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
+
 let pauseBtn = document.getElementById("play-pause");
 let skipNext = document.getElementById("right-btn");
 let skipBack = document.getElementById("left-btn");
 let controller = document.getElementById("controller");
 let music = document.querySelector('Audio');
+
+var library = [{artistName:"TwentyOnePilots", songName:"Ride", Img:"maxresdefault.jpg", src:"ride.mp3"},{artistName:"Twenty One Pilots",
+                songName:"Chlorine", Img:"chlorine.png", src:"Chlorine.mp3"},{artistName:"Dua Lipa", songName:"One Kiss", Img:"dualipa.jpg", src:"One-Kiss.mp3"}]
+
+
+let title = document.getElementById("title");
+let artist = document.getElementById("artist");
+let songImg = document.getElementById("songImg");
+let body = document.querySelector("body");
+let musicSrc = document.querySelector("audio");
+
+
+var position = 0;
+skipNext.addEventListener("click",() => {
+    
+    if(library.length-1 == position){
+        position = 0;
+    }
+    else{
+        position = position + 1;
+    }
+   
+    alert(position)
+    MusicList(position);
+});
+
+
+function MusicList(position){
+    console.log(position)
+    musicSrc.src = "./audio/" + library[position].src;
+    title.innerHTML = library[position].songName;
+    artist.innerHTML = library[position].artistName;
+    songImg.src = "./images/" + library[position].Img;
+    music.play();
+}
+
+MusicList(position);
+
+
+
+
+
+
 
 
 music.onloadedmetadata = function(){
@@ -38,3 +82,11 @@ controller.onchange = function(){
     pauseBtn.classList.remove('fa-play');
 
 }
+
+
+
+
+
+
+
+
